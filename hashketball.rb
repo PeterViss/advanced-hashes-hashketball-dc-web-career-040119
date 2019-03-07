431d710413fd3c9c4de643538c2d746a22b316d4
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-07 19:47:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -204,15 +204,4 @@ def player_stats(name)
 end
 
 def big_shoe_rebounds
-  big_shoe = 0
-  rebounds = 0
-  game_hash.each do |place,team|
-    team[:players].each do |player_name, player|
-    if player[:shoe] > big_shoe
-      big_shoe = player[:shoe]
-      rebounds = player[:rebounds]
-        end
-      end
-    end
-  rebounds
-end
+
